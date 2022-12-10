ed74d695805aa0b45097e21e0529bdc1f3dfaf9a
Sashikanth Raavikanti <Sashikanth.Raavikanti@hotmail.com>
2022-12-10 20:52:26 +0100

    AoC 22 Day 9 Refactored for Part 2

--- src/com/adventofcode2022/day9/RopeBridge.kt
+++ src/com/adventofcode2022/day9/RopeBridge.kt
@@ -5,6 +5,7 @@ import kotlin.math.abs
 var state = State()
 
 fun main() {
+    state.initializeTails(9,0,state.head)
     readDirections()
 }
 
@@ -15,36 +16,36 @@ fun readDirections() {
         updateState(directions[0], directions[1].toInt())
         line = readLine()
     }
-    println("Unique Positions Visited ${state.uniqueTailMovements.size}")
+    println("Unique Positions Visited ${state.tail.uniqueMovements.size}")
 }
 
 fun updateState(s: String, length: Int) {
     when (s) {
-        "R" -> updateState(state.head.first, length, moveRow = false, increment = true)
-        "L" -> updateState(state.head.first, length, moveRow = false, increment = false)
-        "U" -> updateState(length, state.head.second, moveRow = true, increment = true)
-        "D" -> updateState(length, state.head.second, moveRow = true, increment = false)
+        "R" -> updateState(state.head.coordinates.first, length, moveRow = false, increment = true)
+        "L" -> updateState(state.head.coordinates.first, length, moveRow = false, increment = false)
+        "U" -> updateState(length, state.head.coordinates.second, moveRow = true, increment = true)
+        "D" -> updateState(length, state.head.coordinates.second, moveRow = true, increment = false)
     }
 }
 
 fun updateState(x: Int, y: Int, moveRow: Boolean, increment: Boolean) {
     when (moveRow) {
         false -> if (increment) {
-            for (i in state.head.second+1..state.head.second+y) {
+            for (i in state.head.coordinates.second + 1..state.head.coordinates.second + y) {
                 updateHead(x, i)
             }
         } else {
-            for (i in state.head.second-1 downTo state.head.second-y) {
+            for (i in state.head.coordinates.second - 1 downTo state.head.coordinates.second - y) {
                 updateHead(x, i)
             }
         }
-        true -> if(increment) {
-            for (i in state.head.first+1..state.head.first+x) {
-                updateHead(i,y)
+        true -> if (increment) {
+            for (i in state.head.coordinates.first + 1..state.head.coordinates.first + x) {
+                updateHead(i, y)
             }
         } else {
-            for (i in state.head.first-1 downTo state.head.first-x) {
-                updateHead(i,y)
+            for (i in state.head.coordinates.first - 1 downTo state.head.coordinates.first - x) {
+                updateHead(i, y)
             }
         }
 
@@ -53,31 +54,70 @@ fun updateState(x: Int, y: Int, moveRow: Boolean, increment: Boolean) {
 
 
 fun updateHead(x: Int, y: Int) {
-    state.previousHead = state.head
-    state.head = Pair(x, y)
-    updateTail()
+    state.head.coordinates = Pair(x, y)
+    state.head.uniqueMovements.add(state.head.coordinates)
+    updateTail(state.head)
 }
 
-fun updateTail() {
-    val headX = state.head.first
-    val headY = state.head.second
-    val tailX = state.tail.first
-    val tailY = state.tail.second
+fun updateTail(head: Knot) {
+    if (head.isTailInitialized()) {
+        val headX = head.coordinates.first
+        val headY = head.coordinates.second
+        val tailX = head.tail.coordinates.first
+        val tailY = head.tail.coordinates.second
 
-    if(!(abs(headX-tailX)<=1 && abs(headY-tailY)<=1)) {
-        state.tail = when {
-            headX == tailX -> when {headY > tailY -> Pair(tailX,tailY+1) else -> Pair(tailX,tailY-1)}
-            headY == tailY -> when {headX > tailX -> Pair(tailX+1,tailY) else -> Pair(tailX-1,tailY)}
-            else -> state.previousHead
+        if (!(abs(headX - tailX) <= 1 && abs(headY - tailY) <= 1)) {
+            head.tail.coordinates = when {
+                headX == tailX -> when {
+                    headY > tailY -> Pair(tailX, tailY + 1)
+                    else -> Pair(tailX, tailY - 1)
+                }
+                headY == tailY -> when {
+                    headX > tailX -> Pair(tailX + 1, tailY)
+                    else -> Pair(tailX - 1, tailY)
+                }
+                else -> moveDiagonal(head)
+            }
+            head.tail.uniqueMovements.add(head.tail.coordinates)
         }
-        state.uniqueTailMovements.add(state.tail)
+        updateTail(head.tail)
     }
 }
 
 
+fun moveDiagonal(head: Knot): Pair<Int, Int> {
+    val x = when {
+        head.coordinates.first > head.tail.coordinates.first -> head.tail.coordinates.first + 1
+        else -> head.tail.coordinates.first - 1
+    }
+    val y = when {
+        head.coordinates.second > head.tail.coordinates.second -> head.tail.coordinates.second + 1
+        else -> head.tail.coordinates.second - 1
+    }
+    return Pair(x, y)
+}
+
+
 class State {
-    var head = Pair(0, 0)
-    var previousHead = Pair(0,0)
-    var tail = Pair(0, 0)
-    var uniqueTailMovements = mutableSetOf(tail)
+    var head = Knot()
+    lateinit var tail:Knot
+
+    fun initializeTails(maxTails:Int, counter:Int, knot:Knot) {
+        if(counter<maxTails) {
+            knot.tail = Knot()
+            initializeTails(maxTails,counter+1,knot.tail)
+        } else {
+            tail = knot
+        }
+    }
+}
+
+class Knot {
+    var coordinates = Pair(0, 0)
+    var uniqueMovements = mutableSetOf(coordinates)
+    lateinit var tail: Knot
+
+    fun isTailInitialized(): Boolean {
+        return this::tail.isInitialized
+    }
 }
